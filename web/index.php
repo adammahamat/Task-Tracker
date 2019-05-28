47a83fb35e428c44683d73c64b2c2db283386e92
adammahamat <adammahamat493@gmail.com>
2019-05-28 12:00:00 +0300

    change

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', false);
 defined('YII_ENV') or define('YII_ENV', 'prod');
-defined('APP_VER') or define('APP_VER', '2.0');
+defined('APP_VER') or define('APP_VER', '1.0');
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
