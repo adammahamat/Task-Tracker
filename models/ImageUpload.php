1d211b00dac8cc635c2b085801a15db021d5d138
adammahamat <adammahamat493@gmail.com>
2019-06-03 21:08:18 +0300

    add function

--- models/ImageUpload.php
+++ models/ImageUpload.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace app\models;
+use Yii;
 use yii\base\Model;
 
 class ImageUpload extends Model {
 
     public $image;
 
+    public function uploadfile(ImageUpload $file) {
+        $file->saveAs(yii::getAlias('@web').'uploads/'.$file->name);
+    }
 
 }
